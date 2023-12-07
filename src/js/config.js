368d91106b6a45db65d7721de21024c6c818652a
dimetrios <ciobanudumitru1@gmail.com>
2023-12-07 19:02:56 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2605749e-4a0e-4412-b31d-af086fcecf5a';
+export const MODAL_CLOSE_SEC = 2.5;
